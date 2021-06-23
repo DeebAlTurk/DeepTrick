bd04f5d32b33657ced2b33e2a7f0767653bf7886
dturk <dturk@swtichtech.com>
2021-06-23 15:29:04 +0300

    X

--- item_info.py
+++ item_info.py
@@ -1,6 +1,9 @@
-import json
+import time
+
+from selenium import webdriver
 
 import Validtors.url_methods as url
+from Scraping_methods import *
 
 # file = open("Item_links.json")
 # links: dict = json.load(file)
@@ -10,7 +13,29 @@ with open('Item_links.json') as f:
     data = json.load(f)
 base = "https://www.carrefouruae.com"
 count = 0
+links: list = []
+target = {}
+options: list = [0]
 for key in data.keys():
     for i in data[key].split(","):
-        print(url.get_full_url(base, str(i).replace('\'', "").replace('[', "").replace("]", "").replace(" ", '')))
+        links.append(
+            url.get_full_url(base, str(i).replace('\'', "").replace('[', "").replace("]", "").replace(" ", '')))
         count += 1
+browser = webdriver.Firefox()
+count = len(links)
+for link in links:
+    browser.get(link)
+    time.sleep(1)
+    soup = BeautifulSoup(browser.page_source, "lxml")
+    # desc = {'label': str(soup.select_one('css-1i2z667')) + " desc", 'type': 'selector', 'name': '.css-a3q5me', 'exists': True,
+    #         'isRange': 0, 'offset': 0, 'get_what': 'text'}
+    img = {'label': str(soup.select_one('.css-1i2z667').text) + " img", 'type': 'selector',
+           'name': '.swiper-lazy-loading',
+           'exists': True, 'isRange': 1, 'start': 0, 'end': 0, 'get_what': 'attr', 'attr': 'src'}
+    img['end'] = len(soup.select(".swiper-lazy-loading"))
+    # options[0] = desc
+    options[0] = img
+    target = scraper(soup, options, target)
+    count -= 1
+    print(f"{count} left ")
+    convert_dict_to_json(target, name="Desc")
